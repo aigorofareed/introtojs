b77e2fefa688dd6c0bf633a98c66ccbc385c1276
Fareed <aigorolanre@gmail.com>
2022-12-03 12:31:02 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let Para = document.getElementById('p');
+Para.innerHTML = 'Ade is super cool!!!!!';
+let dxd = document.getElementById('cool');
+dxd.innerHTML = 'Fareed is cooler than Ade'
+
+function displayMessage() {
+    tada.innerHTML = 'Ade is the coolest'
+
+}
+function displayMessages(){
+    tadas.innerHTML = 'I am a pro'
+}
